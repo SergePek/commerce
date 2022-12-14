e2d3137a06248e97dda3edff8c0c6d44fbca4c75
SergePek <pekchiorckin@mail.ru>
2022-12-14 20:58:30 +0300

    animation

--- src/data.js
+++ src/data.js
@@ -11,13 +11,13 @@ export const sliderItems = [
         title: "WINTER SALE",
         desc: "dkskkdkls",
         bg: "f5fafd",
-        img: "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+        img: "https://rider-skill.ru/wp-content/uploads/2022/05/pexels-jaime-reimer-2662116-670x470-1.jpg"
     },
     {
         id: 3,
         title: "POPULAR SALE",
         desc: "dkskkdkls",
         bg: "fbf0f4",
-        img: "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+        img: "https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg"
     }
 ]
