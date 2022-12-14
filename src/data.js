eeef5ef878b975f0d5d050da323a8f8a9796188b
SergePek <pekchiorckin@mail.ru>
2022-12-14 20:44:52 +0300

    slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: "SUMMER SALE",
+        desc: "dkskkdkls",
+        bg: "fcf1ed",
+        img: "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+    },
+    {
+        id: 2,
+        title: "WINTER SALE",
+        desc: "dkskkdkls",
+        bg: "f5fafd",
+        img: "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+    },
+    {
+        id: 3,
+        title: "POPULAR SALE",
+        desc: "dkskkdkls",
+        bg: "fbf0f4",
+        img: "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+    }
+]
