e5a85e50e73475747b8aeef06441ba23f1a24539
SergePek <pekchiorckin@mail.ru>
2022-12-15 21:21:56 +0300

    newsletter

--- src/data.js
+++ src/data.js
@@ -38,4 +38,32 @@ export const categories = [
         img: "https://damion.club/uploads/posts/2021-12/1640300703_1-damion-club-p-bozhestvennaya-priroda-priroda-1.jpg",
         title: "LIGHT JACKETS",
     }
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://files.gifts.ru/reviewer/tb/18/11148.30_1_500.jpg",
+    },
+    {
+        id: 2,
+        img:"https://mobimg.b-cdn.net/v3/fetch/9d/9db2d4683d92f5f2045e9142fbd82633.jpeg",
+    },
+    {
+        id: 3,
+        img:"https://kkaravaev.com/wa-data/public/shop/products/53/24/12453/images/20320/20320.1200.jpg",
+    },
+    {
+        id: 4,
+        img:"https://storage-cdn8.gloria-jeans.ru/medias/GKT011148-1-01-300Wx300H.jpg?context=bWFzdGVyfHByb2R1Y3R8NzM2NnxpbWFnZS9qcGVnfGg0OS9oMjAvOTI0Njc5NzA3MDM2Ni9HS1QwMTExNDgtMS0wMV8zMDBXeDMwMEguanBnfDM0YjllZWU1NTFhODFkMWU2MWY3Y2MwZjc1NzdjNTdkYWY1ZTkzZWM0NTVlNmFlMjU0MTk0MzNhOWE0NWQwOWE",
+    },
+    {
+        id: 5,
+        img:"https://lastprint.ru/image/cache/data/products/wear/tshirt_label-600x600.png",
+    },
+    {
+        id: 6,
+        img:"https://printair.ru/uploads/all/de/60/f6/de60f646e842aee04413f592545a3edc/sx-filter__skeeks-cms-components-imaging-filters-Thumbnail/e7305035311f5a76c9108611e0997a9f/futbolki-s-detskim-risunkom.jpg?w=700&h=500&m=inset",
+    }
 ]
+
