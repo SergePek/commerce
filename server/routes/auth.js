987d267bc1e765c3e9535acd47d250f884c247f5
SergePek <pekchiorckin@mail.ru>
2022-12-20 21:30:14 +0300

    auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
     const newUser = new User({
@@ -9,7 +10,7 @@ router.post("/register", async (req, res) => {
         password: CryptoJS.AES.encrypt(
             req.body.password,
             process.env.PASS_SEC
-            ).toString()
+        ).toString()
     });
     try {
         const savedUser = await newUser.save();
@@ -17,7 +18,46 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     };
-    
-})
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user){
+            return res.status(401).json("Wrong credentials!");
+        }
+           
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        if (OriginalPassword !== req.body.password){
+            return res.status(401).json("Wrong credentials!");
+        }
+            
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        )
+        
+        const { password, ...others } = user._doc;
+
+        return res.status(200).json({...others, accessToken});
+
+    } catch (err) {
+
+        return res.status(500).json(err);
+    }
+
+});
+
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,55 @@
+const User = require("../models/User");
+const { verifyTokenAndAdmin, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
+
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+        return res.status(200).json(updatedUser);
+
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        return res.status(500).json("Пользователь успешно удален...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        return res.status(500).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(500).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
