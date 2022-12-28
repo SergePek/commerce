70fdb9cc93a18b24313111704a69ab8c0399c451
SergePek <pekchiorckin@mail.ru>
2022-12-28 21:59:55 +0300

    persist

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { login } from '../redux/apiCalls';
 import { mobile } from "../responsive";
+import {useDispatch, useSelector} from "react-redux"
 
 const Container = styled.div`
     width: 100vw;
@@ -20,7 +22,7 @@ const Wrapper = styled.div`
     width: 25%;
     padding:20px;
     background-color: white;
-    ${mobile({width: "75%"})};
+    ${mobile({ width: "75%" })};
 
 `
 const Form = styled.form`
@@ -46,6 +48,11 @@ const Button = styled.button`
     color: white;
     cursor:pointer;
     margin-bottom: 10px;
+
+    &:disabled{
+        color: green;
+        cursor: not-allowed;
+    }
 `
 
 const Link = styled.a`
@@ -54,16 +61,31 @@ const Link = styled.a`
     text-decoration: underline;
     cursor: pointer;
 `
+const Error = styled.span`
+    color: red;
+`
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+
+    const {isFetching, error} = useSelector(state=> state.user);
+
+
+    const handleClick = (e)=>{
+        e.preventDefault();
+        login(dispatch, {username, password});
+    }
     return (
         <Container>
             <Wrapper>
                 <Title>SIGN IN</Title>
-                <Form> 
-                    <Input placeholder="username" />
-                    <Input placeholder="password" />
-                    <Button>LOGIN</Button>
+                <Form>
+                    <Input placeholder="username" onChange={(e)=>setUsername(e.target.value)}/>
+                    <Input type="password" placeholder="password" onChange={(e)=>setPassword(e.target.value)}/>
+                    <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+                   {error && <Error>Something went wrong...</Error>}
                     <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
                     <Link>CREATE A NEW ACCOUNT</Link>
                 </Form>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,41 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
+import userReduser from "./userRedux";
 
-export default configureStore({
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReduser)
+
+export const store = configureStore({
     reducer: {
         cart: cartReducer,
-    }
+        user: persistedReducer,
+
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+
+
 })
+
+export const persistor = persistStore(store)
